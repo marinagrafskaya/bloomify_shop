c794af83720d6f80db40734bcbc1c7899a439c4f
marinagrafskaya <153319392+marinagrafskaya@users.noreply.github.com>
2024-02-26 13:01:12 +0300

    first

--- assets/connect/db.php
+++ assets/connect/db.php
@@ -2,9 +2,9 @@
 
 define("DB_DRIVER", "mysql");
 define("DB_HOST", "localhost");
-define("DB_NAME", "copystar");
-define("DB_USER", "root");
-define("DB_PASSWORD", "");
+define("DB_NAME", "bloomify_db");
+define("DB_USER", "bloomify_db");
+define("DB_PASSWORD", "0724Bloom");
 
 $db = new \PDO(DB_DRIVER.":host=".DB_HOST.";dbname=".DB_NAME,DB_USER,DB_PASSWORD);
 ?>
